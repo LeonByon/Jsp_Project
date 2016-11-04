e097fdb76b23c4616802eaa9f5b10d520bbc44bc
user <user@DESKTOP-OKDUB61>
2016-11-05 00:54:13 +0900

    게시판v7

--- Jsp_Project/src/board/DAO.java
+++ Jsp_Project/src/board/DAO.java
@@ -72,7 +72,6 @@ public class DAO {
 				}
 				vo.setTime(yea);
 				vo.setHit(rs.getInt(5));
-				vo.setIndent(rs.getInt(6));
 				vo.setDayNew(dayNew);
 				alist.add(vo);
 			}
@@ -91,7 +90,7 @@ public class DAO {
 		ArrayList<VO_COM> alist = new ArrayList<VO_COM>();
 
 		try{
-			sql = "select num,board_num,mem_name,memo,indate from COMMENTS where board_num="+idx+"ORDER BY COMMENTS.NUM";
+			sql = "select num,board_num,mem_name,memo,indate from COMMENTS where board_num="+idx;
 			pstmt = con.prepareStatement(sql);
 			rs = pstmt.executeQuery();
 
@@ -129,7 +128,7 @@ public class DAO {
 		ArrayList<Member> alist = new ArrayList<Member>();
 
 		try{
-			sql = "select num,id,pwd,name,email,phone,indate,admin from MEMBER";
+			sql = "SELECT NUM,ID,PWD,NAME,EMAIL,PHONE,INDATE,ADMIN from MEMBER";
 			pstmt = con.prepareStatement(sql);
 			rs = pstmt.executeQuery();
 
@@ -356,7 +355,7 @@ public class DAO {
 
 		try {
 			sql = "INSERT INTO COMMENTS(board_num, mem_name, memo, indate) "+
-					"VALUES(?,?,?,sysdate)";
+					"VALUES(?,?,?,CURRENT_DATE)";
 			pstmt = con.prepareStatement(sql);
 
 			pstmt.setInt(1, idx);
